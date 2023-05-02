a4583e18275086302cb26f28ddda3ae58d971438
samira kamal <samirakamal2001@gmail.com>
2023-05-01 21:16:10 -0400

    commit files to repo

--- cproject.js
+++ cproject.js
@@ -1,21 +1,8 @@
-const { body } = document;
-
-let zoomActivated = false;
-
-window.addEventListener('keydown', (e) => {
-    if (e.key === 'z') {
-        zoomActivated = !zoomActivated;
-    }
-});
-
-window.addEventListener('mousemove', (e) => {
-    const { clientX: x, clientY: y } = e;
-
-    if (zoomActivated) {
-        body.style.transform = `scale(2)`;
-        body.style.transformOrigin = `${x}px ${y}px`;
-    } else {
-        body.style.transform = 'none';
+var input = document.getElementById("body");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 27) {
+        event.preventDefault();
+        document.documentElement.scrollTop = 0;
     }
 });
 
