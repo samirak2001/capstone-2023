50ac9510b38ade0c2128bae646fa8e99f4689679
samira kamal <samirakamal2001@gmail.com>
2023-05-01 23:30:24 -0400

    commit files to repo

--- cproject.js
+++
@@ -1,8 +0,0 @@
-var input = document.getElementById("body");
-input.addEventListener("keyup", function(event) {
-    if (event.keyCode === 27) {
-        event.preventDefault();
-        document.documentElement.scrollTop = 0;
-    }
-});
-
